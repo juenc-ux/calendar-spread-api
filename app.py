1027e3f9e6b2dc6e5aa223a11bef691f5171be9a
juenc-ux <nils.juergens@web.de>
2025-10-19 21:46:03 +0200

    Revert "Add selectable multi-theme dashboard mockup"

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify
 from flask_cors import CORS
 import yfinance as yf
 
 app = Flask(__name__)
 CORS(app)
-
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
 
 @app.route('/api/stock/<ticker>', methods=['GET'])
 def get_stock_data(ticker):
